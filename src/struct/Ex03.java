8e960e5bfbaa2cd48848aa00591278b5bd89a61f
khhckss@naver.com <khhckss@naver.com>
2022-04-22 00:18:35 +0900

    fix

--- src/struct/Ex03.java
+++ src/struct/Ex03.java
@@ -16,18 +16,19 @@ public class Ex03 {
 		
 		return top;
 	}
-	static void showList() {	//지금들어가있는 Stack의 데이터를 보여주는 작업
+	static void showList(Stack top) {	//지금들어가있는 Stack의 데이터를 보여주는 작업
 		while(true) {
-			if(top.link != null) {
+			if(top.link == null) {
 				System.out.println(top.data);
-			}
-			else if(top.link == null){
-				System.out.println(top.data);
-			}
+				break;
+			 }
+			int tmp = top.data;
+			System.out.println(tmp);
 			top = top.link;
-			break;
+			
 			}
 		}
+	
 	static void Pop() {
 		top = top.link;
 		
@@ -43,16 +44,22 @@ public class Ex03 {
 		System.out.println(top.link.data);
 		System.out.println(top.link.link.data);
 		System.out.println();
-		showList();
+		showList(top);
+		
+		Push(10);
+		Push(20);
+		Push(30);
 		
 		Pop();
-		showList();
+		System.out.println();
+		System.out.println(top.data);
+		showList(top);
 		System.out.println();
 		Pop();
-		showList();
+		showList(top);
 		System.out.println();
 		Pop();
-		showList();
+		showList(top);
 		
 		
 	}
