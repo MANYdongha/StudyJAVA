e69045b0322b8e93a7add6f771d5542215ea0845
khhckss@naver.com <khhckss@naver.com>
2022-04-25 21:11:25 +0900

    add

--- src/string/Test.java
+++ src/string/Test.java
@@ -27,28 +27,28 @@ public class Test {
 	}
 	result = "12345";
 	System.out.println("123"+ result.charAt(0));
+	
+//	5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
+	if(result.equals("")) {
+		result = "a";
+	}
+	System.out.println(result);
+//	6단계 new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
+//     만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
+	if(result.length() >= 15) {
+		result = result.substring(0, 14);				
+		if(result.charAt(result.length()-1) == '.') {
+			result.substring(0, 13);
+		}
+	}
+	System.out.println("1"+result);
+//	7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.	
+	while(result.length() <= 2) {
+		char ch = result.charAt(result.length()-1);
+		result += ch;
+	}
 //	
-////	5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
-//	if(result.equals("")) {
-//		result = "a";
-//	}
-//	System.out.println(result);
-////	6단계 new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
-////     만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
-//	if(result.length() >= 15) {
-//		result = result.substring(0, 14);				
-//		if(result.charAt(result.length()-1) == '.') {
-//			result.substring(0, 13);
-//		}
-//	}
-//	System.out.println("1"+result);
-////	7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.	
-//	while(result.length() <= 2) {
-//		char ch = result.charAt(result.length()-1);
-//		result += ch;
-//	}
-////	
-//	System.out.println(result);
+	System.out.println(result);
 
 	
 	}
