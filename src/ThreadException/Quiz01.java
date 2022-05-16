436ef1756249e85856ec1fcc09d354a48aeab2d1
khhckss@naver.com <khhckss@naver.com>
2022-05-17 00:30:39 +0900

    add

--- src/ThreadException/Quiz01.java
+++ src/ThreadException/Quiz01.java
@@ -1,8 +1,12 @@
 package ThreadException;
 
+import java.util.Random;
+import java.util.Scanner;
+
 class Timer1 implements Runnable{
-	int Seccend;
-	
+	private int Seccend;
+	private Quiz q;
+	private boolean over;
 	
 	
 	
@@ -10,21 +14,105 @@ class Timer1 implements Runnable{
 		this.Seccend = seccend;
 	}
 
+	
+	
+	public int getSeccend() {
+		return Seccend;
+	}
+
+
+
+	public void setSeccend(int seccend) {
+		Seccend = seccend;
+	}
+
+
+
+	public Quiz getQ() {
+		return q;
+	}
+
+
+
+	public void setQ(Quiz q) {
+		this.q = q;
+	}
+
+	public boolean isOver() {
+		return over;
+	}
+
+	public void setOver(boolean over) {
+		this.over = over;
+	}
+
+
+
 	@Override
 	public void run() {
-		
+		int i;
+		for(i=Seccend; i!= -1 && q.isOver() == false ; i--) {
+			try {
+				System.out.printf("남은시간 : [%02d : %02d]\n", i / 60, i % 60);
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		over = true;
+		if(i ==	 -1) {
+			System.out.println("시간초과1");			
+		}
+		System.exit(0);
 	}
 }
 //생성자로  second를 전달받아서 run()에서 0초까지 출력하는 클래스
 class Quiz implements Runnable{
+	Random ran = new Random();
+	Scanner sc = new Scanner(System.in);
+	private int Seccend;
+	private Timer1 t; 
+	private boolean over;
+	
+	
+	public int getSeccend() {
+		return Seccend;
+	}
+
+	public void setSeccend(int seccend) {
+		Seccend = seccend;
+	}
+
+	public Timer1 getT() {
+		return t;
+	}
+
+	public void setT(Timer1 t) {
+		this.t = t;
+	}
 
 	
 	
-	
-	
-	
+	public boolean isOver() {
+		return over;
+	}
+
+	public void setOver(boolean over) {
+		this.over = over;
+	}
+
 	@Override
 	public void run() {
+		int num1  = ran.nextInt(9)+1; 
+		int num2  = ran.nextInt(9)+1;
+		int sum = num1 + num2;
+		System.out.printf("%d + %d = \n",num1,num2);
+		int result = sc.nextInt();
+		over = true;
+//		if(t.isOver()) {
+//			System.out.println("시간초과2");
+//		}
+		System.out.println(result == sum ? "정답" : "오답");
 		
 	}
 }
@@ -38,9 +126,17 @@ public class Quiz01 {
 		//타이머를 5초로 해서 객체를 생성
 		//수식퀴즈 객체 생성
 		
+		t.setQ(q);
+		q.setT(t);
+		
+		Thread th1 = new Thread(t);
+		Thread th2 = new Thread(q);
 		
 		//수식퀴즈의 start를 호출하여 식을 보여주고 답을 입력받기
 		//타이머 5초 start
+		th2.start();
+		th1.start();
+		
 		
 		//시간안에 정답을 입력하먄 정답, 시간이 초과되거나, 답이 틀리면 오답출력
 		
